30c4ff4f797e0f267ebb28894fc9931c3cc5f977
jffryan <jffryan@gmail.com>
2024-07-31 18:19:11 -0500

    push up latest changes

--- resources/js/views/EditBookView.vue
+++ resources/js/views/EditBookView.vue
@@ -5,7 +5,7 @@
   </div>
   <div v-else>
     <h1>Edit [{{ currentBook.title }}]</h1>
-    <BookCreateEditForm :current-book="currentBook" class="w-1/2" />
+    <div></div>
   </div>
 </template>
 
@@ -14,13 +14,9 @@ import { useBooksStore } from "@/stores";
 
 import { getOneBookFromSlug } from "@/api/BookController";
 
-import BookCreateEditForm from "@/components/books/forms/BookCreateEditForm.vue";
-
 export default {
   name: "EditBookView",
-  components: {
-    BookCreateEditForm,
-  },
+
   setup() {
     const BooksStore = useBooksStore();
 
