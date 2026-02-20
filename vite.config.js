d3bacac4e7b368537028eddcbcafee5118baa350
Jeff Ryan <jffryan@gmail.com>
2026-02-19 21:31:12 -0600

    Moves laravel packages into their own container for performance

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
+const port = Number(process.env.VITE_PORT || 5173)
+
 export default defineConfig({
     plugins: [
         vue(),
@@ -10,4 +12,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        port,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            port: port,
+        },
+    },
 });
